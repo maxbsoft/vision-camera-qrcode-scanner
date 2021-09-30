444fa912373bb1d828bb055effd877a5f28c20e1
Max Balukh <maxbsoft@gmail.com>
2021-10-01 02:40:08 +0300

    fix ean13

--- ios/VisionCameraQrcodeScanner.swift
+++ ios/VisionCameraQrcodeScanner.swift
@@ -19,40 +19,16 @@ class VisionCameraQrcodeScanner: NSObject, FrameProcessorPluginBase {
            let barcodes: [Barcode] =  try barcodeScanner.results(in: image)
            if (!barcodes.isEmpty){
                 for barcode in barcodes {
-                   var map: [String: Any] = [:]
-                   switch barcode.valueType {
-                   case .URL:
-                       map["title"] = barcode.url?.title
-                       map["url"] = barcode.url?.url
-                   case .wiFi:
-                       map["password"] = barcode.wifi?.password
-                       map["encryptionType"] = barcode.wifi?.type
-                       map["ssid"] = barcode.wifi?.ssid
-                   default:
-                    
-                        map["displayValue"] = barcode.displayValue
-                        // map["valueType"] = barcode.valueType
-                        map["rawValue"] = barcode.rawValue
-                        // map["rawData"] = String(decoding: barcode.rawData, as: UTF8.self)
-                        
-
-                        if (barcode.format == .code128) {
-                            map["barcodeType"] = "code128"
-                        } else if (barcode.format == .code128) {
-                            map["barcodeType"] = "code39"
-                        } else if (barcode.format == .code93) {
-                            map["barcodeType"] = "code93"
-                        } else if (barcode.format == .codeBar) {
-                            map["barcodeType"] = "codeBar"
-                        } else if (barcode.format == .EAN13) {
-                            map["barcodeType"] = "EAN13"
-                        } else if (barcode.format == .EAN8) {
-                            map["barcodeType"] = "EAN8"
-                        }
-                        
-                   }
-                   barCodeAttributes.append(map)
-                 }
+                    var map: [String: Any] = [:]
+                    map["displayValue"] = barcode.displayValue
+                    map["rawValue"] = barcode.rawValue
+                    map["frameMinX"] = barcode.frame.minX
+                    map["frameMinY"] = barcode.frame.minY
+                    map["frameWidth"] = barcode.frame.width
+                    map["frameHeight"] = barcode.frame.height
+                    map["barcodeType"] = "EAN13"
+                    barCodeAttributes.append(map)
+                }
            }
          } catch _ {
                return nil
